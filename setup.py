e62e4ddac63fb3200ad0bc7f51fe36eccdca77b3
Callum Lamont <cal_lamont@hotmail.com>
2019-09-01 21:36:12 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="simpycirc",
     version="0.0.1",
     description="To easily simulate electrical circuits",
-    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=["docs", "tests"]),
     install_requires=["matplotlib>=3.0", "scipy>=1.2", "pandas>=0.24.2"],
+    url="https://github.com/calamont/simpycirc",
+    author="Callum Lamont",
+    email="cal_lamont@hotmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
